9f2a9e4d1baf96b3412f0b1c3c6f184fafc505f4
unknown <mohmmedtouhid007@gmail.com>
2022-11-08 01:18:37 +0600

    num-obj

--- main.js
+++ main.js
@@ -1,53 +1,40 @@
-// var ownerName = document.getElementById("owner_name");
-// var flatNumber = document.getElementById("flat_number");
-// var currentDate = document.getElementById("date");
+var ownerName = document.getElementById("owner_name");
+var flatNumber = document.getElementById("flat_number");
+var currentDate = document.getElementById("date");
 
+const ownerNameTwo = document.querySelector("[data-owner-name]");
+const flatNumberTwo = document.querySelector("[data-flat-number]");
 
-// const ownerNameTwo = document.querySelector('[data-owner-name]')
-// const flatNumberTwo = document.querySelector('[data-flat-number]')
+var houseBill = document.querySelector("[data-houseBill-amount]");
+var serviceBill = document.querySelector("[data-serviceBill-amount]");
+var waterBill = document.querySelector("[data-waterBill-amount]");
+var totalAmount = document.querySelector("[data-total-amount]");
+var gasBill = document.getElementById("gas_bill");
+var electricityBill = document.getElementById("electricity_bill");
 
+var total = houseBill + serviceBill;
+console.log(total);
 
+gasBill.addEventListener("blur", function () {
+  gasBill.innerText = this.value;
+});
 
+ownerName.addEventListener("keyup", function () {
+  ownerNameTwo.innerText = this.value;
+});
 
-// var houseBill = document.querySelector('[data-housebill-amount]')
-// var serviceBill = document.querySelector('[data-servicebill-amount]')
-// var waterBill = document.querySelector('[data-waterbill-amount]')
-// var totalAmount = document.querySelector('[data-total-amount]')
-// var gasBill = document.getElementById('gas_bill')
-// var electricityBill = document.getElementById('electricity_bill')
-
-
-// var totalBill = parseInt(houseBill.value + serviceBill.value + waterBill.value) 
-
-//  console.log(totalBill);
-// totalAmount.innerText = totalBill;
-// console.log(totalAmount)
-
-
-// ownerName.addEventListener("keyup", function () {
-//     var value = this.value;
-//     console.log(value);
-//     ownerNameTwo.innerText = this.value;
-// }); 
-
-
-// flatNumber.addEventListener("keyup", function () {
-//     var value = this.value;
-//     console.log(value);
-//     flatNumberTwo.innerText = this.value;
-// }); 
-
+flatNumber.addEventListener("keyup", function () {
+  flatNumberTwo.innerText = this.value;
+});
 
 // star here
 
-let $el = document.querySelector('.electricity-bill');
-if( $el ) {
-    $el.addEventListener('blur', function() {
-        let $HouseBill = document.getElementsByClassName("house-bill").textContent;
-        console.log('HouseBill', $HouseBill);
-        let $ServiceBill = '';
-        let $WaterBill = '';
-    });
-}
-
-
+// let $el = document.querySelector('.electricity-bill');
+// if( $el ) {
+//     $el.addEventListener('blur', function() {
+//         let $HouseBill = document.getElementsByClassName("house-bill").textContent;
+//         console.log('HouseBill', $HouseBill);
+//         let $ServiceBill = '';
+//         let $WaterBill = '';
+//     });
+// }
